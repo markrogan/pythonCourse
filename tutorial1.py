f21892fb5619c85d387ad15e2d6f79a2b7c7681d
markrogan <marogan@tcd.ie>
2018-02-18 18:04:13 +0000

    tutorial one finished

--- tutorial1.py
+++ tutorial1.py
@@ -1,5 +1,24 @@
-myList 	= [1, 2, 3]
+myList = [1, 2, 3]
+
 myTuple = (1, 2, 3)
 
-for i in myTuple:
-	print i
+myList.pop(1)
+
+
+myDict = {}
+
+myDict['table'] = 'A four legged furniture object'
+myDict['chair'] = 'A place to rest your bum'
+
+
+myFile = open('/Users/mark/git/pythonCourse/files/firstText.txt', 'w')
+myFile.write('out test')
+
+
+# for i in myTuple:
+#     print i
+
+# for i in myList:
+#     print i
+
+print myDict['table']
